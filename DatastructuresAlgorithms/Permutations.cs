896d367badae94decef0a5ff161e0a8e806a22a2
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-06-02 14:43:21 +0530

    [vijayalakshmi]: calculate permutations

---
+++ DatastructuresAlgorithms/Permutations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatastructuresAlgorithms
+{
+    internal class Permutations
+    {
+            public void Permute(String s, String answer)
+            {
+                if (s.Length == 0)
+                {
+                    Console.Write(answer + "  ");
+                    return;
+                }
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    char ap = s[i];
+                    String left_substr = s.Substring(0, i);
+                    String right_substr = s.Substring(i + 1);
+                    String result = left_substr + right_substr;
+                    Permute(result, answer + ap);
+                }
+            }
+    }
+}
+
---
+++ DatastructuresAlgorithms/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+namespace DatastructuresAlgorithms
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            String s;
+            String answer = "";
+
+            Console.Write("Enter the string: ");
+            s = Console.ReadLine();
+
+            Console.Write("\nAll possible strings are : ");
+            Permutations value = new Permutations();
+            value.Permute(s, answer);
+
+        }
+    }
+}
